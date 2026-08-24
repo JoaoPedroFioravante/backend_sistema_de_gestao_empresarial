502f3f50ce908ffb26f6c0ca54bb69af9f713c6c
Joao Fioravante <joaogalleni1376@gmail.com>
2026-08-24 20:49:12 -0300

    correção schema cliente

--- src/utils/clientSchemaPost.js
+++ src/utils/clientSchemaPost.js
@@ -4,15 +4,7 @@ const clientSchema = z.object({
     name: z.string().min(3, "o tamanho minimo do nome é 3 caracteres").max(20, "o tamanho maximo do nome é 20 caracteres"),
     lastName: z.string().min(3,  "o tamanho minimo do nome é 3 caracteres").max(20,  "o tamanho maximo do nome é 20 caracteres"),
     cpf : z.string().length(11, "o cpf deve ter 11 caracteres exatamente"),
-    email: z.email("email invalido"),
-    password: z.string().min(8, "tamanho minimo da senha é 8 caracteres").max(20, "tamanho maximo da senha é 20 caracteres"),
-    confirmPassword: z.string(),
     contactNumbers: z.array(z.string("os elementos do campo telefone devem ser do tipo texto").length(12, "o telefone deve conter 12 digitos, 11 numeros e o traço separador")).min(1, "deve conter pelo menos 1 numero valido").optional()
-}).refine((data)=>{
-    return data.password === data.confirmPassword,{
-        error: "senhas não coincidem",
-        path: ["confirmPassword"]
-    }
 })
 .refine((data)=>{
     let isValidContactNumbers =  true;
