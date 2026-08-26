ac7b8b5341733d67244a5cd4ef477d85d86cab65
joao <joaogalleni1376@gmail.com>
2026-08-25 22:50:16 -0300

    controller de pagamentos estrutura básica

--- src/controllers/PayoutController.js
+++ src/controllers/PayoutController.js
@@ -1,18 +1,50 @@
-export default class PayoutController{
-    constructor(){
+export default class PayoutController {
+  constructor() {}
 
+  getAll = async (req, res, next) => {
+    try {
+      let allPayouts; //... vai vir do banco
+      res.status(200).json({ message: "sucess", payouts: "payouts 123" });
+    } catch (e) {
+      next(e);
     }
-
-    getAll = async(req, res, next)=>{
-
+  };
+  getOne = async (req, res, next) => {
+    const payoutId = req.params.id;
+    try {
+      let client; //vai vir do banco
+      res
+        .status(200)
+        .json({ message: "sucess", payout: `payout de id ${payoutId}` });
+    } catch (e) {
+      next(e);
     }
-    getOne = async(req, res, next)=>{
-
+  };
+  post = async (req, res, next) => {
+    const body = req.body;
+    try {
+      res.status(200).json({ message: "sucess", body: { body } });
+    } catch (e) {
+      next(e);
     }
-    post = async(req, res, next)=>{
-
+  };
+  patch = async (req, res, next) => {
+    const body = req.body;
+    const idPayout = req.params.id;
+    try {
+      res
+        .status(200)
+        .json({ message: "sucess", body: { body }, idPayout: idPayout });
+    } catch (e) {
+      next(e);
     }
-    patch = async(req, res, next)=>{
-
+  };
+  delete = async (req, res, next) => {
+    const idPayout = req.params.id;
+    try {
+        res.status(200).json({message:`payout of id ${idPayout} was deleted with sucess`});
+    } catch (e) {
+      next(e);
     }
+  };
 }
--- src/routes/payoutRoutes.js
+++ src/routes/payoutRoutes.js
@@ -1,10 +1,11 @@
 import e from 'express'
 import validatorPatchSchemaPayout from '../middlewares/validatorPatchSchemaPayout.js';
 import validatorPostSchemaPayout from '../middlewares/validatorPostSchemaPayout.js';
+import PayoutController from '../controllers/PayoutController.js';
 const router = e.Router();
-let controller;
+let controller =  new PayoutController(); 
 
-router.get("/", controller.get);
+router.get("/", controller.getAll);
 router.get("/:id", controller.getOne);
 router.post("/" ,validatorPostSchemaPayout, controller.post);
 router.patch("/:id", validatorPatchSchemaPayout, controller.patch);
