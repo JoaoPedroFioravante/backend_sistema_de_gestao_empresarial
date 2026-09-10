4207bbff7c3dbb9034807e1ce23632b119d6f89b
joaoFioravante <joaogalleni1376@gmail.com>
2026-09-10 15:59:38 -0300

    criação do controller de estoque

---
+++ src/controllers/StockController.js
@@ -0,0 +1,63 @@
+export default class StockController {
+  constructor() {}
+  getAll = async (req, res, next) => {
+    try {
+      let products; //vem do banco
+      res.status(200).json({ message: "sucesso", products: "produtos" });
+    } catch (e) {
+      next(e);
+    }
+  };
+
+  getOne = async (req, res, next) => {
+    const productId = req.params.id;
+    try {
+      let product; //vem do banco
+      res
+        .status(200)
+        .json({ message: "sucesso", product: `id do produto ${productId}` });
+    } catch (e) {
+      next(e);
+    }
+  };
+
+  patch = async (req, res, next) => {
+    const productId = req.params.id;
+    const productModification = req.body;
+    try {
+      res.status(200).json({
+        message: "sucess",
+        product: productModification,
+        productId: productId,
+      });
+    } catch (e) {
+      next(e);
+    }
+  };
+
+  post = async (req, res, next) => {
+    const product = req.body;
+    try{
+        res.status(201).json({
+            message:"sucess",
+            product: product
+        });
+    }
+    catch(e){
+        next(e);
+    }
+  };
+
+  remove = async (req, res, next) => {
+    const productId = req.params.id;
+    try{
+        res.status(200).json({
+            message:"sucess",
+            product_id: `id do produto deletado ${productId}`
+        });
+    }
+    catch(e){
+        next(e);
+    }
+  };
+}
