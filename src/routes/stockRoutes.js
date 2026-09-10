3641baab8e4d701c60986cbf2fb6819c7ff5d918
joaoFioravante <joaogalleni1376@gmail.com>
2026-09-10 16:48:41 -0300

    controller e validators foram adicionados a stock routes

--- src/routes/stockRoutes.js
+++ src/routes/stockRoutes.js
@@ -1,13 +1,15 @@
 import express from "express";
-import StockController from '../controllers/StockController.js';
-import validatorPostSchemaStock from "../middlewares/validatorPostSchemaStock.js"
-import validatorPatchSchemaStock from "../middlewares/validatorPatchSchemaStock.js"
+import validatorPatchSchemaStock from '../middlewares/validatorPatchSchemaStock.js'
+import validatorPostSchemaStock from '../middlewares/validatorPostSchemaStock.js'
+import StockController from '../controllers/StockController.js'
+
 const router = express.Router();
 const controller = new StockController();
+
 router.get("/", controller.getAll);
 router.get("/:id", controller.getOne);
-router.patch("/:id", validatorPatchSchemaStock, controller.patch);
+router.patch("/:id",validatorPatchSchemaStock, controller.patch);
 router.post("/", validatorPostSchemaStock, controller.post);
-router.delete("/:id", controller.delete);
+router.delete("/:id", controller.remove);
 
 export default router;
